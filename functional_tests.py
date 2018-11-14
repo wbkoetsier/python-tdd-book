31aafda7b55fbb1daf54833c28244be1f94dc7ab
Wil Bruins-Koetsier <wbkoetsier@users.noreply.github.com>
2018-11-14 11:17:05 +0100

    Refactor ft for dry

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,21 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):  
         self.browser.quit()
+    
+    def input_todo_item(self, item_text: str=''):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys(item_text)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+    def check_for_row_in_list_table(self, row_text: str=''):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Elisabeth has heard about a cool new online to-do app. She goes to 
@@ -29,38 +44,19 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
         # She types "Feed the ducks" into a text box 
-        inputbox.send_keys('Feed the ducks')  
-
         # When she hits enter, the page updates, and now the page lists
         # "1: Feed the ducks" as an item in a to-do list.
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-        table = WebDriverWait(self.browser, DEFAULT_WAIT).until(
-            EC.presence_of_element_located((By.ID, 'id_list_table'))
-        )
-        # table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Feed the ducks', [row.text for row in rows])
+        self.input_todo_item('Feed the ducks')
+        self.check_for_row_in_list_table('1: Feed the ducks')
 
         # There is still a text box inviting her to add another item. She enters 
         # "Don't forget to close the fence" (we can't have the ducks wander off 
         # onto the street).
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Don\'t forget to close the fence')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
-
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2: Don\'t forget to close the fence', [row.text for row in rows])
+        # The page updates again, and now shows both items on her list.
+        self.input_todo_item('Don\'t forget to close the fence')
+        self.check_for_row_in_list_table('2: Don\'t forget to close the fence')
+        
         self.fail('Finish the test!')
 
         # Elisabeth wonders whether the site will remember her list. Then she 

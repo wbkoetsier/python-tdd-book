37fe98245fe9a2f4908e7847550df1e3fd108811
Wil Bruins-Koetsier <wbkoetsier@users.noreply.github.com>
2018-11-14 16:56:11 +0100

    Add new url and refactor templates, views and tests
    New url for posting list items to. That's the thing being a
    REST-purist... Adding this url kinda hurts. But I get the point, it gets
    things working, it's not supposed to be REST, it's supposed to be
    REST-ish. Bear with the author HP here.

--- lists/tests.py
+++ lists/tests.py
@@ -27,26 +27,24 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
     
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'lists/home.html')
+        
+
+class NewListTest(TestCase):
+
     def test_can_save_a_POST_request(self):
         item_text = 'A new list item'
-        self.client.post('/', data={'item_text': item_text})
+        self.client.post('/lists/new', data={'item_text': item_text})
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, item_text)
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-    
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        self.assertTemplateUsed(response, 'lists/home.html')
-        
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-    
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
 
 class ListViewTest(TestCase):
     
--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,14 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'lists/home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists/list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
